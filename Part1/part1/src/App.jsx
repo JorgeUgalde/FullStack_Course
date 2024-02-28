1ca5516e7b66a4698031e4db5f95f07feffb9df9
JorgeLuis <jorgeluisu10@gmail.com>
2024-02-28 11:10:44 -0600

    Parte D antes de los ejercicios

--- Part1/part1/src/App.jsx
+++ Part1/part1/src/App.jsx
@@ -77,7 +77,7 @@
 // ************************************************************************************************************//
 // Re-renderizar el componente cuando cambia algo
 
-import { useState } from "react";
+// import { useState } from "react";
 
 // const App = () => {
 //   const [ counter, setCounter ] = useState(0)
@@ -92,24 +92,133 @@ import { useState } from "react";
 //   )
 // }
 
-const App = () => {
-  const [counter, setCounter] = useState(0);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+// const App = () => {
+//   const [counter, setCounter] = useState(0);
+
+//   const increaseByOne = () => setCounter(counter + 1);
+//   const decreaseByOne = () => setCounter(counter - 1);
+//   const setToZero = () => setCounter(0);
+
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button onClick={increaseByOne} text="plus" />
+//       <Button onClick={setToZero} text="zero" />
+//       <Button onClick={decreaseByOne} text="minus" />
+//     </div>
+//   );
+// };
+// const Display = ({ counter }) => <div>{counter}</div>;
+
+// const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+
+
+// ************************************************************************************************************//
+// parte D
+
+// import { useState } from "react";
+
+// const App = () => {
+//   const [clicks, setClicks] = useState({
+//     left: 0, right: 0
+//   })
+
+//   // guardar cada uno de los clicks por separado 
+
+//   // const handleLeftClick = () => {
+//   //   const newClicks = {
+//   //     left: clicks.left + 1,
+//   //     right: clicks.right
+//   //   }
+//   //   setClicks(newClicks)
+//   // }
+
+//   // const handleRightClick = () => {
+//   //   const newClicks = {
+//   //     left: clicks.left,
+//   //     right: clicks.right + 1
+//   //   }
+//   //   setClicks(newClicks)
+//   // }
+
+//   // Copiar los clicks y solo aumentar el que se ocupa aumentar
+//   const handleLeftClick = () =>
+//   setClicks({ ...clicks, left: clicks.left + 1 })
+
+// const handleRightClick = () =>
+//   setClicks({ ...clicks, right: clicks.right + 1 })
+
+//   return (
+//     <div>
+//       {clicks.left}
+//       <button onClick={handleLeftClick}>left</button>
+//       <button onClick={handleRightClick}>right</button>
+//       {clicks.right}
+//     </div>
+//   )
+// }
+
+// ************************************************************************************************************//
+// Parte D, Manejo de matrices 
+import React, { useState } from 'react'
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updateRight = right + 1
+    setRight(updateRight)
+    setTotal(left + updateRight)
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+      <p>total {total}</p>
     </div>
-  );
-};
-const Display = ({ counter }) => <div>{counter}</div>;
+  )
+}
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+// renderizado condicional 
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 
 export default App;
